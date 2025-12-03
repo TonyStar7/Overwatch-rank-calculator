e921888f8eb3e5583149aace87e21f8f6cb910be
Tony <tonyyang749@gmail.com>
2025-12-03 22:01:02 +0100

    added delete button

--- Src/main.py
+++ Src/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import requests
+import json
 
 class MyWindow(Tk):
     def __init__(self):
@@ -39,26 +40,25 @@ class MyWindow(Tk):
         refresh_button.grid(row=0, column=2, sticky="nsew")
 
 
-        #self.list of acc
+        #list of acc
         self.list_frame = Frame(self, bg="#858585", highlightbackground="white", highlightthickness=2)
         self.list_frame.grid(row=1, column=1, columnspan=1, sticky="new")      
-        for column in range(6):
+        for column in range(7):
             self.list_frame.grid_columnconfigure(column, weight=1)
-        for row in range(4):
-            self.list_frame.grid_rowconfigure(row, weight=1)
+        
 
         game = Label(self.list_frame, text="Game", fg="White", bg=self.bg_lightgray, font=("Calibri", 15))
-        game.grid(column=0, row=0, padx=10)
+        game.grid(column=1, row=0, padx=10)
         acc = Label(self.list_frame, text="Account", fg="White", bg=self.bg_lightgray, font=("Calibri", 15))
-        acc.grid(column=1, row=0, padx=40)
+        acc.grid(column=2, row=0, padx=40)
         tank = Label(self.list_frame, text="Tank", fg="White", bg=self.bg_lightgray, font=("Calibri", 15))
-        tank.grid(column=2, row=0, padx=40)
+        tank.grid(column=3, row=0, padx=40)
         dps = Label(self.list_frame, text="DPS", fg="White", bg=self.bg_lightgray, font=("Calibri", 15))
-        dps.grid(column=3, row=0, padx=40)
+        dps.grid(column=4, row=0, padx=40)
         supp = Label(self.list_frame, text="Support", fg="White", bg=self.bg_lightgray, font=("Calibri", 15))
-        supp.grid(column=4, row=0, padx=40)
+        supp.grid(column=5, row=0, padx=40)
         owner = Label(self.list_frame, text="Owner", fg="White", bg=self.bg_lightgray, font=("Calibri", 15))
-        owner.grid(column=5, row=0, padx=40)
+        owner.grid(column=6, row=0, padx=40)
 
 
         self.add_entry.bind("<FocusIn>", self.add_entry_click_delete)
@@ -68,6 +68,7 @@ class MyWindow(Tk):
         self.title("Overwatch Rank Calculator")
         self.configure(bg="#333333", padx=20, pady=20)
         self.geometry("1600x800")
+        
 
 
 # Function to get rank for each role
@@ -83,13 +84,15 @@ class MyWindow(Tk):
         
 
     def add_acc_to_list(self, player_id, player_data):
-        account_name = player_data["username"]
 
+        # Call the info
+        account_name = player_data["username"]
         tank = self.rank_info(player_data, "tank")
         damage = self.rank_info(player_data, "damage")
         support = self.rank_info(player_data, "support")
         owner = "N/A"
 
+        # Put infos in a list
         row_data = [
             "OW",
             account_name,
@@ -100,13 +103,15 @@ class MyWindow(Tk):
         ]
 
         curr_row = self.list_row_counter
-        fg_text="White"
 
+        # Create new row for each acc
         for col, content in enumerate(row_data):
+            button = Button(self.list_frame, text=X, fg="White", bg=self.bg_lightgray, font=("Calibri", 15), command=lambda r=curr_row: self.delete_acc(r))    #delete button
+            button.grid(row=curr_row, column=0, sticky="nsew")
             label = Label(self.list_frame, text=content, fg="White", bg=self.bg_lightgray, font=("Calibri", 15))
-            label.grid(row=curr_row, column=col, sticky="nsew")
+            label.grid(row=curr_row, column=col+1, sticky="nsew")
         self.list_row_counter += 1
-        self.self.list_frame.grid_rowconfigure(curr_row, weight=1)
+        self.list_frame.grid_rowconfigure(curr_row, weight=1)
 
 
 # Function when add button is clicked
@@ -122,6 +127,16 @@ class MyWindow(Tk):
             self.add_acc_to_list(player_id, player_data)
         else:
             print("Failed to retrieve player data.")
+    
+
+    #delete row in acc list
+    def delete_acc(self, row):
+        for widget in self.list_frame.winfo_children():
+            grid_data = widget.grid_info()
+            if grid_data.get('row') == row:
+                widget.destroy()
+            
+
 
     #auto delete default text of entry when clicked
     def add_entry_click_delete(self, event):
@@ -139,6 +154,9 @@ class MyWindow(Tk):
         if widget != self.add_entry:
             self.add_entry_default_text(event)
             self.focus()
+
+    
+    
     
     #API PART
     BASE_URL = "https://overfast-api.tekrop.fr"
